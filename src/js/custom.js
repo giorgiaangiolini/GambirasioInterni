931cc96379230d314043a194d335d7b8092ccc7a
Giorgia Angiolini <giorgiaangiolini.info@gmail.com>
2022-11-22 19:29:08 +0100

    done servizi e contatti senza animazioni

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,10 +1,10 @@
 import Swiper from 'swiper/bundle';
-
 import 'swiper/css/bundle';
 
 
-  // slideshow
-  const swiper = new Swiper('.swiper', {
+  function init(){
+// slideshow
+const swiper = new Swiper('#swiperHp', {
   loop: true,
   slidesPerView: 1,
   draggable: true,
@@ -13,7 +13,7 @@ import 'swiper/css/bundle';
   centeredSlides: true,
   effect: "fade",
   autoplay: {
-    delay: 4500,
+    delay: 4000,
     disableOnInteraction: false,
   },
   // navigation: {
@@ -24,12 +24,49 @@ import 'swiper/css/bundle';
    slideChangeTransitionStart: function () {
        let activeSlide = this.el.querySelector('div.swiper-slide-active');
        let caption = activeSlide.getAttribute("data-caption");
-       console.log(caption)
+       let captionTitle = activeSlide.getAttribute("data-title");
        let slideCaption = document.querySelector(".slide-captions");
        if(slideCaption != null){
-         slideCaption.innerHTML = "<p class='current-title'>" + caption + "</p>"
+         slideCaption.innerHTML = "<p class='slide-title'>" + captionTitle + "<p class='current-title'>" + caption + "</p>"
        }
    }
  }
 
 });
+
+  // slideshowGalleria
+  const swiperGalleria = new Swiper('#swiperGalleria', {
+    loop: true,
+    slidesPerView: 1,
+    draggable: true,
+    grabCursor: true,
+    spaceBetween: 0,
+    centeredSlides: true,
+    effect: "fade",
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    // navigation: {
+    //   nextEl: '.swiper-button-next',
+    //   prevEl: '.swiper-button-prev',
+    // },
+    on: {
+     slideChangeTransitionStart: function () {
+         let activeSlide = this.el.querySelector('div.swiper-slide-active');
+         let caption = activeSlide.getAttribute("data-caption");
+         let captionTitle = activeSlide.getAttribute("data-title");
+         let slideCaption = document.querySelector(".slide-captions");
+         if(slideCaption != null){
+           slideCaption.innerHTML = "<p class='slide-title'>" + captionTitle + "<p class='current-title'>" + caption + "</p>"
+         }
+     }
+   }
+  
+  });
+
+  }
+
+  init()
+
+  
