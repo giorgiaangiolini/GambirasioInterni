53d92995d4a017096072fad7fa23bdfc04e94730
Giorgia Angiolini <giorgiaangiolini.info@gmail.com>
2022-11-23 11:52:35 +0100

    sistemati swiper fatte animazioni per active link

--- src/js/custom.js
+++ src/js/custom.js
@@ -2,39 +2,45 @@ import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
 
-  function init(){
-// slideshow
-const swiper = new Swiper('#swiperHp', {
-  loop: true,
-  slidesPerView: 1,
-  draggable: true,
-  grabCursor: true,
-  spaceBetween: 0,
-  centeredSlides: true,
-  effect: "fade",
-  autoplay: {
-    delay: 4000,
-    disableOnInteraction: false,
-  },
-  // navigation: {
-  //   nextEl: '.swiper-button-next',
-  //   prevEl: '.swiper-button-prev',
-  // },
-  on: {
-   slideChangeTransitionStart: function () {
-       let activeSlide = this.el.querySelector('div.swiper-slide-active');
-       let caption = activeSlide.getAttribute("data-caption");
-       let captionTitle = activeSlide.getAttribute("data-title");
-       let slideCaption = document.querySelector(".slide-captions");
-       if(slideCaption != null){
-         slideCaption.innerHTML = "<p class='slide-title'>" + captionTitle + "<p class='current-title'>" + caption + "</p>"
-       }
-   }
- }
+// slideshow homepage
 
-});
+function initHome(){
+  const swiper = new Swiper('#swiperHp', {
+    loop: true,
+    slidesPerView: 1,
+    draggable: true,
+    grabCursor: true,
+    spaceBetween: 0,
+    centeredSlides: true,
+    effect: "fade",
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    // navigation: {
+    //   nextEl: '.swiper-button-next',
+    //   prevEl: '.swiper-button-prev',
+    // },
+    on: {
+     slideChangeTransitionStart: function () {
+         let activeSlide = this.el.querySelector('div.swiper-slide-active');
+         let caption = activeSlide.getAttribute("data-caption");
+         let captionTitle = activeSlide.getAttribute("data-title");
+         let slideCaption = document.querySelector(".slide-captions");
+         if(slideCaption != null){
+           slideCaption.innerHTML = "<p class='slide-title'>" + captionTitle + "<p class='current-title'>" + caption + "</p>"
+         }
+     }
+   } 
+  });
+}
 
-  // slideshowGalleria
+initHome()
+
+
+// slideshow galleria
+
+function initGalleria(){
   const swiperGalleria = new Swiper('#swiperGalleria', {
     loop: true,
     slidesPerView: 1,
@@ -64,9 +70,53 @@ const swiper = new Swiper('#swiperHp', {
    }
   
   });
+}
 
-  }
 
-  init()
+// Tabs navbar
+
+let navTabs = document.querySelectorAll(".nav-tab");
+
+navTabs.forEach((item)=>{
+  let tabId = item.getAttribute("tab");
+  item.addEventListener("click", function(evt){
+      let i;
+      let x = document.getElementsByClassName("section");
+      for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+        navTabs[i].classList.remove("nav-active");
+      }
+      document.getElementById(tabId).style.display = "block";
+      evt.target.classList.add("nav-active");
+
+      
+      if(tabId == "hp"){initHome()}
+      if(tabId == "galleria"){initGalleria()}
+
+  });
+})
+
+// Tabs servizi
+
+let serviziTabs = document.querySelectorAll(".tablink");
+
+serviziTabs.forEach((item)=>{
+  let serviceId = item.getAttribute("tab");
+
+  item.addEventListener("click", function(evt){
+    let i;
+    let x = document.getElementsByClassName("service");
+
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+      serviziTabs[i].classList.remove("tab-active");
+    }
+
+    document.getElementById(serviceId).style.display = "block";
+    evt.target.classList.add("tab-active");
+  })
+
+})
+
 
   
